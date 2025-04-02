9b513d27f6da507fe4864b55d50598ad07175752
Ruxanda Lungu <adnax@MacBook-Pro-de-Ruxanda-Work.local>
2025-04-02 19:59:39 +0100

    Fix localeDetection value in i18n config

--- next.config.js
+++ next.config.js
@@ -1,17 +1,11 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
-    // These are all the locales you want to support in your application
     locales: ['pt', 'en', 'ro'],
-    // This is the default locale you want to be used when visiting
-    // a non-locale prefixed path e.g. `/hello`
     defaultLocale: 'pt',
-    // This is a list of locale domains and the default locale they
-    // should handle (these are only required when setting up domain routing)
-    // Note: subdomains must be included in the domain value to be matched e.g. "fr.example.com".
-    localeDetection: true,
+    localeDetection: false, // ← IMPORTANTE: sem aspas!
   },
   reactStrictMode: true,
+  swcMinify: true,
 };
 
-export default nextConfig;
+module.exports = nextConfig;
